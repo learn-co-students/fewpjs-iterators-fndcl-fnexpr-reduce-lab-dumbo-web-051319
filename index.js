2da3e808836babeab70cc432c7d2705f1883439c
L.R.L <DaleLoreNY@gmail.com>
2019-07-07 19:16:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+console.log("connected")
+
+let totalBatteries = batteryBatches.reduce(function(total, battery){
+  return total + battery
+}, 0)
+
+console.log(totalBatteries)
